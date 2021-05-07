14d3297428854920c7c85a5715ef07b397c0a8cd
nozomi-hasei <nozominozonozo@hotmail.co.jp>
2021-05-07 16:16:44 +0900

    モデルアソシエーション

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,4 @@
 class Tag < ApplicationRecord
+  has_many :tag_user_relation
+  has_many :user
 end
--- app/models/tag_user_relation.rb
+++ app/models/tag_user_relation.rb
@@ -1,2 +1,4 @@
 class TagUserRelation < ApplicationRecord
+  belongs_to :user
+  belongs_to :tag
 end
