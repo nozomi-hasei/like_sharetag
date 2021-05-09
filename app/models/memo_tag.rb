4684cfda03641edd207ae0b0f2fee938f3cf091f
nozomi-hasei <nozominozonozo@hotmail.co.jp>
2021-05-09 16:24:04 +0900

    ルーティング　FORMオブジェクト

--- app/models/memo_tag.rb
+++ app/models/memo_tag.rb
@@ -1,14 +1,17 @@
 class MemoTag 
 
-  include ActiveModel::module 
+  include ActiveModel::Module 
   attr_accessor :memo, :name
     
   with_options presence: true do
     validates :memo
-    validates :name
+    validates :tag
   end
 
   def save
-    memo = Memo.create()
+    memo = Memo.create(memo_content: memo_content)
+    tag = Tag.create(tag_name: tag_name)
+
+    MemoTagRelation.create(memo_id: memo.id, tag_id: tag.id)
   end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,5 @@
 class Tag < ApplicationRecord
   has_many :memo_tag_relations
   has_many :memos, through: :memo_tag_relations
+
 end
