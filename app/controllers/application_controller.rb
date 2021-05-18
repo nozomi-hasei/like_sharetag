4afe6b9745a80337bb44d718d3cfb7cb5eddef2d
nozomi-hasei <nozominozonozo@hotmail.co.jp>
2021-05-18 11:05:26 +0900

    Revert "Merge pull request #1 from nozomi-hasei/ユーザー管理機能"
    This reverts commit c040883977c051a82fcfec4dc30e5d219ee58ee4, reversing
    changes made to 29d76551b13fc365e2c87e552210ed6adbbea0af.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  #テーブルに追加したカラムの許可
-  private
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :name_kana, :nickname])
-  end
-  
-  #ユーザー登録バリデーション
-  with_options presence: true do
-    with_options format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
-      validates :name
-    end 
-    with_options format: {with: /\A[ァ-ヶー－]+\z/} do
-      validates :name_kana
-    end 
-    validates :nickname 
-    validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "can't be registered"}
-  end
 end
-
